354d46a2de475acdd2134aa077ed171bc80f3016
Viacheslav Klovan <slava.klovan@gmail.com>
2025-06-14 20:42:30 +0200

    Implement common cognito module

---
+++ terraform/cognito/variables.tf
@@ -0,0 +1,9 @@
+variable "user_pool_name" {
+  description = "Name of the Cognito user pool."
+  type        = string
+}
+
+variable "user_pool_client_name" {
+  description = "Name of the Cognito user pool client."
+  type        = string
+}
