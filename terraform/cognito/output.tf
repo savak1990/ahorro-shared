8da384b1901d574c7cb89832b1ee4debe2669e74
Viacheslav Klovan <slava.klovan@gmail.com>
2025-06-21 11:42:12 +0200

    Add arn to the output of cognito module

--- terraform/cognito/output.tf
+++ terraform/cognito/output.tf
@@ -7,3 +7,8 @@ output "user_pool_client_id" {
   description = "The ID of the Cognito User Pool Client."
   value       = aws_cognito_user_pool_client.this.id
 }
+
+output "user_pool_arn" {
+  description = "The ARN of the Cognito User Pool."
+  value       = aws_cognito_user_pool.this.arn
+}
